e0061f1b4e24d6c35a9656dedfd20ca23debdb0a
Koziolek <bjkuczynski@gmail.com>
2019-12-22 14:48:15 +0100

    git ignore

--- src/test/java/pl/koziolekweb/pwm/properties/SimpleCalculatorPropertiesTest.java
+++ src/test/java/pl/koziolekweb/pwm/properties/SimpleCalculatorPropertiesTest.java
@@ -3,6 +3,7 @@ package pl.koziolekweb.pwm.properties;
 import net.jqwik.api.Example;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
+import net.jqwik.api.constraints.Negative;
 import net.jqwik.api.constraints.Positive;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -21,8 +22,13 @@ class SimpleCalculatorPropertiesTest implements AutoCloseable{
 	}
 
 	@Property
-	void simplePositiveProperties(@ForAll @Positive() int a, @ForAll @Positive int b) {
+	void simplePositiveProperties(@ForAll @Positive int a, @ForAll @Positive int b) {
 		assertThat(sut.sum(a, b)).isGreaterThanOrEqualTo(a).isGreaterThanOrEqualTo(b);
+	}
+
+	@Property
+	void simpleNegativeProperties(@ForAll @Negative int a, @ForAll @Negative int b) {
+		assertThat(sut.sum(a, b)).isLessThanOrEqualTo(a).isLessThanOrEqualTo(b);
 	}
 
 
